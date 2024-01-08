de884f70dc4bcb47e9eabfd744287642d953e6c7
Omercanonen <117309573+Omercanonen@users.noreply.github.com>
2024-01-08 19:42:03 +0300

    Update Personel.cs

--- Projeclass/Personel.cs
+++ Projeclass/Personel.cs
@@ -12,47 +12,8 @@ namespace Projeclass
     public class Personel
     {
 
-        //public static List<Personel> personellistesi = new List<Personel>();
-
-        //public string PersonelAdi;
-        //public string PersonelSifre;
-
-        //public Personel(string PersonelAdi,string PersonelSifre) 
-        //{
-        //    this.PersonelAdi = PersonelAdi;
-        //    this.PersonelSifre = PersonelSifre;
-
-        
-        //}
-
-        //public void ListeyePersonelEkle(Personel personel)
-        //{
-        //    personellistesi.Add(personel);
-        //}
-
-        //public static bool GirisKontrol(string personelAdi, string personelSifre)
-        //{
-        //    foreach (Personel personel in personellistesi)
-        //    {
-        //        if (personel.PersonelAdi == personelAdi && personel.PersonelSifre == personelSifre)
-        //        {
-        //            return true; 
-        //        }
-        //    }
-
-        //    return false; 
-        //}
-
         public string personeladi {  get; set; }
         public string personelsifre { get; set; }
-
-
-
-
-
-
-
-
 
     }
 }
